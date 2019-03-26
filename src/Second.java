66b827eb86df4c662e0178c5b2c97ec299e4654f
ravi <ravikushwaharavi198@gmail.com>
2019-03-26 09:40:36 +0530

    first commit

---
+++ src/Second.java
@@ -0,0 +1,43 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Second
+ */
+@WebServlet("/second")
+public class Second extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Second() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		Cookie ck[]=request.getCookies();
+		
+		out.print("hello : "+ck[0].getValue());
+		out.close();
+	}
+
+}
